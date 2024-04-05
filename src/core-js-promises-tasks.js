5c986957a8a5dcdb333083c73e9370eff96d32f0
onqqr <dnbyaniss@gmail.com>
2024-04-05 16:47:39 +0300

    feat: solved queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -146,8 +146,17 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  let sumPromises = Promise.resolve();
+  promises.forEach((promise) => {
+    sumPromises = sumPromises.then(() =>
+      promise.then((value) => {
+        result += value;
+      })
+    );
+  });
+  return sumPromises.then(() => result);
 }
 
 module.exports = {
